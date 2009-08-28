aadae32654d4583f2417deaae5965098343988bb
Kyle Maxwell <kyle@kylemaxwell.com>
2009-08-28 15:37:53 -0700

    Version bump to 0.2.0

---
+++ lib/jsonget.rb
@@ -0,0 +1,28 @@
+require "rubygems"
+require "rwget"
+require "parsley"
+require "activesupport"
+require "fileutils"
+
+class JSONStore
+  def initialize(options = {})
+    @output_folder = options[:prefix] || "."
+    FileUtils.mkdir_p(@output_folder)
+    @parselets = (options[:parselets] || []).map{|path| Parsley.new(File.read(path)) }
+    @files     = (options[:parselets] || []).map{|path| File.open("#{File.basename(path)}.json", "a") }
+  end
+
+  def put(host, tmpfile)
+    @parselets.zip(@files).each do |parselet, file|
+      begin
+        file.puts parselet.parse(:file => tmpfile.path, :output => :json) + ","
+      rescue ParsleyError => e
+        STDERR.puts "warning: #{e.message}"
+      end
+    end
+  end
+
+  def close
+    @files.map(&:close)
+  end
+end
